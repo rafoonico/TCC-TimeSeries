0a6bbb114469f31525f179faa8ddf5d7cfaf1e04
Rafael Buttini Salviato <rbsalviato@gmail.com>
2020-03-16 20:37:31 -0300

    Add files via upload

---
+++ projeto_TCC_Igor_Horrocks_e_Rafael_Salviato.R
@@ -0,0 +1,18 @@
+# Para inserir sessões no código: Ctrl + Shift + R
+# Computador do Rafa ------------------------------------------------------
+setwd("C:/Users/rafae/Documents/GitHub/TCC-TimeSeries")
+# Computador do Igor ------------------------------------------------------
+
+
+# 1) Função geradora de séries temporais ----------------------------------
+# É preciso fazer uma função que gere as séries e armazene em formato
+# de lista. A função deve ter pelo menos três argumentos:
+# - Um dataframe com os parâmetros para a série temporal
+# - Tamanho da série
+# - Número de repetições
+#
+#
+# - Para gerar a série temporal, é mais fá
+
+
+
