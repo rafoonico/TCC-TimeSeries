e6410195e104dfd5ad389d301d516c0d681dad8f
Rafael Buttini Salviato <rbsalviato@gmail.com>
2020-03-19 16:57:53 -0300

    Add files via upload

--- projeto_TCC_Igor_Horrocks_e_Rafael_Salviato.R
+++ projeto_TCC_Igor_Horrocks_e_Rafael_Salviato.R
@@ -1,19 +1,19 @@
-# Para inserir sessÃµes no cÃ³digo: Ctrl + Shift + R
+# Para inserir sessões no código: Ctrl + Shift + R
 # Computador do Rafa ------------------------------------------------------
 setwd("C:/Users/rafae/Documents/GitHub/TCC-TimeSeries")
 # Computador do Igor ------------------------------------------------------
 
-# 1) FunÃ§Ã£o geradora de sÃ©ries temporais ----------------------------------
-# Ã‰ preciso fazer uma funÃ§Ã£o que gere as sÃ©ries e armazene em formato
-# de lista. A funÃ§Ã£o deve ter pelo menos trÃªs argumentos:
-# - Um dataframe com os parÃ¢metros para a sÃ©rie temporal
-# - Tamanho da sÃ©rie
-# - NÃºmero de repetiÃ§Ãµes
+# 1) Função geradora de séries temporais ----------------------------------
+# É preciso fazer uma função que gere as séries e armazene em formato
+# de lista. A função deve ter pelo menos três argumentos:
+# - Um dataframe com os parâmetros para a série temporal
+# - Tamanho da série
+# - Número de repetições
 
 geradora_ARMA <- function(parametros,n,m,order_arma="ARMA"){
-# 1) parÃ¢metros: data frame com os parÃ¢metros
-# 2) n: tamanho de cada sÃ©rie temporal
-# 3) m: nÃºmero de simulaÃ§Ãµes
+# 1) parâmetros: data frame com os parâmetros
+# 2) n: tamanho de cada série temporal
+# 3) m: número de simulações
   library(magrittr)
   
   if(order_arma=="ARMA"){
@@ -79,4 +79,90 @@ parametros_func <- function(p,d,q,AR=NULL,MA=NULL){
   return(resultado)
 }
 
-# sugestÃ£o de alteraÃ§Ã£o
+
+# 2) Testando o pacote 'strucchange" --------------------------------------
+# Vou apenas indicar as sessões do artigo onde os códigos se encontram.
+
+# "3 - The Data"
+
+library(strucchange):library(magrittr)
+
+data("USIncExp")
+ts.plot(USIncExp, gpars = list(col = c("black", "red"))) : legend("topleft", 
+                                                                  legend=c("income","expenditure"),
+                                                                  col = c("black", "red"), lty=1)
+USIncExp2 <- window(USIncExp,start=c(1985,12))
+coint.res <- residuals(lm(expenditure~income,data=USIncExp2)) %>% 
+                       ts(start=c(1985,12),freq=12) %>%
+                       lag(k=-1)
+USIncExp2 <- cbind(USIncExp2, diff(USIncExp2), coint.res)
+USIncExp2 <- window(USIncExp2, start=c(1986,1), end=c(2001,2))
+colnames(USIncExp2) <- c("income","expenditure","diff.income","diff.expenditure","coint.res")
+ecm.model <- diff.expenditure~coint.res+diff.income
+
+# "4 - Generalized fluctuation tests"
+
+ocus <- efp(ecm.model, type="OLS-CUSUM", data=USIncExp2)
+me <- efp(ecm.model, type="ME", data=USIncExp2, h=0.2)
+
+# "4.2 - Boundaries and plotting"
+
+bound.ocus <- boundary(ocus, alpha=0.05)
+plot(ocus)
+plot(ocus, boundary=FALSE) # It is also possible to suppress the boundaries and add them afterwards e.g. another color
+lines(bound.ocus, col=4)
+lines(-bound.ocus, col=4)
+plot(me, functional =NULL)
+
+# "4.3 - Significance testing with empirical fluctuation processes"
+
+sctest(ocus)
+sctest(ecm.model, type="OLS-CUSUM", data=USIncExp2)
+
+# "5.1 - F Statistics: function FStats"
+
+fs <-  Fstats(ecm.model, from = c(1990,1), to = c(1999,6), data = USIncExp2)
+
+# "5.2 - Boundaries and plotting"
+
+plot(fs) : plot(fs, pval=TRUE) : plot(fs, aveF=TRUE)
+
+# "5.3 - Significance testing with F statistics"
+
+sctest(fs, type="expF")
+sctest(ecm.model, type = "expF", from=49, to=162, data=USIncExp2)
+
+# "6 - Monitoring with the generalized fluctuation test"
+
+USIncExp3 <- window(USIncExp2, start = c(1986,1), end =c(1989,12))
+me.mefp <- mefp(ecm.model, type = "ME", data = USIncExp3, alpha=0.05)
+
+USIncExp3 <- window(USIncExp2, start = c(1986,1), end =c(1990,12))
+me.mefp <- monitor(me.mefp)
+
+USIncExp3 <- window(USIncExp2, start = c(1986,1))
+me.mefp <- monitor(me.mefp)
+
+me.mefp
+
+USIncExp3 <- window(USIncExp2, start=c(1986,1), end=c(1989,12))
+me.efp <- efp(ecm.model, type="ME", data=USIncExp3, h=0.5)
+me.mefp <- mefp(me.efp, alpha=0.05)
+
+USIncExp3 <- window(USIncExp2, start=c(1986,1))
+me.mefp <- monitor(me.mefp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
